c237534e2ebf2fd1b60977377e00f43dd08ecb7c
Patrick Roche <rochep63@gmail.com>
2022-04-19 22:23:22 -0400

    add icon, tweak login/register pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "root")
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "bootstrap"),
+        os.path.join(BASE_DIR, "assets")
 ]
 
 # Default primary key field type
